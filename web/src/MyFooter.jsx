4781a8ecae2478574854bb1884392fee515743b4
VenkataSaiKumar531 <injarapu.sai@gmail.com>
2021-10-25 09:50:23 +0530

    Create MyFooter.jsx
    Added footer in the project

---
+++ web/src/MyFooter.jsx
@@ -0,0 +1,12 @@
+import CopyRight from "./CopyRight";
+const MyFooter = () => {
+  return (
+    <footer style={{ background: "Linen" }}>
+      <p>Author: Hactoberfest</p>
+      <a href="https://hacktoberfest.digitalocean.com/">Website</a>
+      <CopyRight />
+    </footer>
+  );
+};
+
+export default MyFooter;
